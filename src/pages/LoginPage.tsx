3768442cbc7df185ef3b74828b30b571849f25da
Deepu Kunjumon <deepukunjumon595@gmail.com>
2026-06-20 23:32:10 +0530

    Updated missed changes while app name updation

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -43,7 +43,7 @@ function AppLogo({ size = 'md' }: { size?: 'sm' | 'md' }) {
           <path d="M21 18v1c0 1.1-.9 2-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h14c1.1 0 2 .9 2 2v1h-9a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h9zm-9-2h10V8H12v8zm4-2.5c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5z" />
         </svg>
       </div>
-      <span className={`text-white font-semibold ${textSize} tracking-tight`}>FinTrack</span>
+      <span className={`text-white font-semibold ${textSize} tracking-tight`}>Cashlytics</span>
     </div>
   );
 }
@@ -254,7 +254,7 @@ function LoginPage() {
               <path d="M21 18v1c0 1.1-.9 2-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h14c1.1 0 2 .9 2 2v1h-9a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h9zm-9-2h10V8H12v8zm4-2.5c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5z" />
             </svg>
           </div>
-          <span className="text-xl font-bold tracking-tight">FinTrack</span>
+          <span className="text-xl font-bold tracking-tight">Cashlytics</span>
         </div>
 
         <div className="w-full max-w-sm space-y-6">
